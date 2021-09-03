1d742689fe4d924e9dc4755c3f7c2127464acb77
Cameron Weston <cameronbweston@gmail.com>
2021-09-03 14:10:31 -0500

    Add and create another model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from main_app.models import Subscription
 from django.contrib import admin
-from .models import Subscription
+from .models import Purchase, Subscription
 
 # Register your models here.
 admin.site.register(Subscription)
+admin.site.register(Purchase)
--- main_app/models.py
+++ main_app/models.py
@@ -11,4 +11,14 @@ class Subscription(models.Model):
 
   def get_absolute_url(self):
       return reverse("subs_detail", kwargs={"sub_id": self.id})
+
+class Purchase(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(max_digits=4, decimal_places=2)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+      return reverse("purchases_detail", kwargs={"purchase_id": self.id})
   
