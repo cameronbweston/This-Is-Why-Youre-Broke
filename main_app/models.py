428c6efa71e512952cfa043da1f815dff492131b
Cameron Weston <cameronbweston@gmail.com>
2021-09-03 12:42:13 -0500

    Get app up and running. Get all subs page and home page and premium page rendering

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Subscription(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.DecimalField(max_digits=4, decimal_places=2)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+      return reverse("subs_detail", kwargs={"sub_id": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Subscription
 
 # Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def premiumcontent(request):
+  return render(request, 'premiumcontent.html')
+
+def subs_index(request):
+  subs = Subscription.objects.all()
+  return render(request, 'subscriptions/index.html', {'subs' : subs})
+
+def subs_detail(request, sub_id):
+  sub = Subscription.objects.get(sub_id = sub_id)
+
+class SubCreate(CreateView):
+  model = Subscription
+  fields = '__all__'
+  success_url = '/subscriptions/'
+
+class SubUpdate(UpdateView):
+  model = Subscription
+  fields = '__all__'
+
+class SubDelete(DeleteView):
+  model = Subscription
+  success_url = '/subscriptions/'
