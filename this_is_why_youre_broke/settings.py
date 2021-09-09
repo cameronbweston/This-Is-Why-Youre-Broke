45dcfb22ddc4dd6c0e0b3dd77272c7766509310c
Cameron Weston <cameronbweston@gmail.com>
2021-09-09 09:39:34 -0500

    Config deployment

--- this_is_why_youre_broke/settings.py
+++ this_is_why_youre_broke/settings.py
@@ -19,6 +19,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
+WSGI_APPLICATION = 'catcollector.wsgi.application'
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
